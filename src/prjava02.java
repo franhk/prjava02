69727a791db921a0324327f22069f9cdfe1e52b2
fortega <fortega@FORTEGA.mem.local>
2019-05-08 18:45:24 +0200

    Treballant amb ordres Git i branques: Versió 1.3 del projecte prjava02

--- src/prjava02.java
+++ src/prjava02.java
@@ -16,7 +16,10 @@ public class prjava02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("versió 0.1 del projecte prjava02"); 	
+        System.out.println("versió 0.1 del projecte prjava02"); 
+        System.out.println("Creació d'una branca del projecte prjava02");
+		System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+		System.out.println("Afegint codi a la branca01 del projecte prjava02");
         try {
             InetAddress adreça = InetAddress.getLocalHost();
             String hostname = adreça.getHostName();
